09112e073666c975629957f8d315eae3b850660a
elena <elenalistratova11@gmail.com>
2017-08-07 16:47:15 +0300

    2: .scaleLinear

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-document.body.innerHTML = d3.version;
+var linearScale = d3.scaleLinear()
+    .domain([0, 100])
+    .range([0, 600])
+    .clamp(true);
+
+// console.log(linearScale(0));
+// console.log(linearScale(50));
+// console.log(linearScale(150));
+// console.log(linearScale.invert(300));
