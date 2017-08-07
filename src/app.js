90786dbe212d82dfa246178e5ce435760547a485
elena <elenalistratova11@gmail.com>
2017-08-07 16:59:36 +0300

    3: .scaleTime

--- src/app.js
+++ src/app.js
@@ -7,3 +7,11 @@ var linearScale = d3.scaleLinear()
 // console.log(linearScale(50));
 // console.log(linearScale(150));
 // console.log(linearScale.invert(300));
+
+
+var timeScale = d3.scaleTime()
+    .domain([new Date(2016, 0, 1), new Date()])
+    .range([0, 100]);
+
+// console.log(timeScale(new Date(2016, 3, 15)));
+// console.log(timeScale.invert(50));
